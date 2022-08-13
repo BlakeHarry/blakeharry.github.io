6f7d0f197ed31f49e97f394a1c8b982461ef4a0c
BlakeHarry <blake.harry@hotmail.com>
2022-08-13 07:36:35 +0100

    c

--- index.py
+++ index.py
@@ -1,9 +1,20 @@
+from re import X
 import pyodide
 
 print("Hello Harry from .py")
 
 def my_function(e):
-    outputHandle.innerText = int(inputHandle.value) * 2
+    x = inputHandle.value
+    try:
+        x = int(x)
+    except:
+        outputHandle.innerText = "This should be a number!"
+    else:
+        if x % 2 == 0:
+            outputHandle.innerText = "This is an even number!"
+        else:
+            outputHandle.innerText = "This is an odd number"
+    
 
 changeButtonHandle = document.getElementById('change_me')
 
